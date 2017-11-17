5a813bc266cf65e6670f015dd217f6522a96bc9e
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-11-17 14:34:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ module.exports = function AutoVanguard(dispatch) {
 
 	let enable = true
 
+	// code
+	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (event) => {
+		if (!enable) return
+
+		dispatch.toServer('C_COMPLETE_DAILY_EVENT', { id: event.id })
+
+		// method 1 : rudimentary method
+		// tries to complete weekly and daily bonuses without check
+		try {
+			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', { type: 0 }) }, 500)
+			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', { type: 1 }) }, 500)
+		} catch (e) {
+			// do nothing
+		}
+		return false
+	})
+
+	// method 2 : use of S_AVAILABLE_EVENT_MATCHING_LIST
+	// requires user to open Vanguard UI will
+	// will not automatically collect weekly and daily bonuses without doing so
+	/* 
+	dispatch.hook('S_AVAILABLE_EVENT_MATCHING_LIST', event => {
+		if (event.unk5 == 3 || event.unk5 == 8) {
+			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })}, 500)
+		} else if (event.unk6 == 16) {
+			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })}, 500)
+		}
+	})
+	*/
+
+	// command
 	try {
 		const Command = require('command')
 		const command = Command(dispatch)
@@ -27,35 +58,5 @@ module.exports = function AutoVanguard(dispatch) {
 	} catch (e) {
 		console.log(`[ERROR] -- auto-vanguard module --`)
 	}
-
-	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, (event) => {
-
-		if (!enable) return
-
-		dispatch.toServer('C_COMPLETE_DAILY_EVENT', { id: event.id })
-
-		// method 1 : rudimentary method
-		// tries to complete weekly and daily bonuses without check
-		try {
-			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 }) }, 500)
-			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 }) }, 500)
-		} catch (e) {
-			// do nothing
-		}
-		return false
-	})
-
-	// method 2 : use of S_AVAILABLE_EVENT_MATCHING_LIST
-	// requires user to open Vanguard UI will
-	// will not automatically collect weekly and daily bonuses without doing so
-	/* 
-	dispatch.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 1, event => {
-		if (event.unk5 == 3 || event.unk5 == 8) {
-			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })}, 500)
-		} else if (event.unk6 == 16) {
-			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })}, 500)
-		}
-	})
-	*/
 
 }
