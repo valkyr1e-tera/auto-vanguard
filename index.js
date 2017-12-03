c25709d269efc6a27f84d07897a058edb5bbabba
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-12-03 17:47:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@
 // - S_COMPLETE_EVENT_MATCHING_QUEST
 // - Optional : S_AVAILABLE_EVENT_MATCHING_LIST
 
-// Version 1.2r01
+// Version 1.3 r:01
 
-module.exports = function AutoVanguard(dispatch) {
+module.exports = function AutoVanguard(d) {
 
 	let enable = true
 
 	// code
-	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (event) => {
+	d.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (e) => {
 		if (!enable) return
 
-		dispatch.toServer('C_COMPLETE_DAILY_EVENT', { id: event.id })
+		d.toServer('C_COMPLETE_DAILY_EVENT', { id: e.id })
 
 		// method 1 : rudimentary method
 		// tries to complete weekly and daily bonuses without check
 		try {
-			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', { type: 0 }) }, 500)
-			setTimeout(() => { dispatch.toServer('C_COMPLETE_EXTRA_EVENT', { type: 1 }) }, 500)
+			setTimeout(() => { d.toServer('C_COMPLETE_EXTRA_EVENT', { type: 0 }) }, 500)
+			setTimeout(() => { d.toServer('C_COMPLETE_EXTRA_EVENT', { type: 1 }) }, 500)
 		} catch (e) {
 			// do nothing
 		}
@@ -31,11 +31,11 @@ module.exports = function AutoVanguard(dispatch) {
 	// requires user to open Vanguard UI will
 	// will not automatically collect weekly and daily bonuses without doing so
 	/* 
-	dispatch.hook('S_AVAILABLE_EVENT_MATCHING_LIST', event => {
-		if (event.unk5 == 3 || event.unk5 == 8) {
-			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })}, 500)
-		} else if (event.unk6 == 16) {
-			setTimeout(() => {dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })}, 500)
+	d.hook('S_AVAILABLE_EVENT_MATCHING_LIST', (e) => {
+		if (e.unk5 == 3 || e.unk5 == 8) {
+			setTimeout(() => {d.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })}, 500)
+		} else if (e.unk6 == 16) {
+			setTimeout(() => {d.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })}, 500)
 		}
 	})
 	*/
@@ -43,14 +43,14 @@ module.exports = function AutoVanguard(dispatch) {
 	// command
 	try {
 		const Command = require('command')
-		const command = Command(dispatch)
+		const command = Command(d)
 		// KR
 		command.add('지령', () => {
 			enable = !enable
 			send(`${enable ? '<font color="#56B4E9">실행</font>' : '<font color="#E69F00">중지</font>'}<font>되었습니다.</font>`)
 		})
 		// NA
-		command.add(['vg', 'vanguard'], () => {
+		command.add(['vg', 'vanguard', 'ㅍㅎ'], () => {
 			enable = !enable
 			send(`${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}<font>.</font>`)
 		})
