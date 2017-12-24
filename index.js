7768dd335535ae488ff027fec661df0eeeb18f00
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2017-12-24 15:16:47 +0900

    Update index.js -- 1.33 Updated code aesthetics

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 // - C_COMPLETE_EXTRA_EVENT
 // - S_COMPLETE_EVENT_MATCHING_QUEST
 
-// Version 1.32 r:00
+// Version 1.33 r:00
 
 module.exports = function AutoVanguard(d) {
 
 	let enable = true
+	
+	/*
+	// for self ; commented out for regular usage
+	d.hook('S_LOGIN', (e) => {
+		(((e.templateId - 10101) % 100) != 5) ? enable = true : enable = false
+	})
+	*/
 
 	// code
 	d.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (e) => {
@@ -31,14 +38,14 @@ module.exports = function AutoVanguard(d) {
 		// KR
 		command.add('지령', () => {
 			enable = !enable
-			send(`${enable ? '실행되었습니다'.clr('56B4E9') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+			send(`${enable ? '실행되었습니다'.clr('5AFF39') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
 		// NA
 		command.add(['vg', 'vanguard', 'ㅍㅎ'], () => {
 			enable = !enable
-			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+			send(`${enable ? 'enabled'.clr('5AFF39') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
-		function send(msg) { command.message(`[auto-vanguard] : ` + msg) }
+		function send(msg) { command.message(`[auto-vanguard] : ` + [...arguments].join('\n\t - ')) }
 	} catch (e) { console.log(`[ERROR] -- auto-vanguard module --`) }
 
 }
