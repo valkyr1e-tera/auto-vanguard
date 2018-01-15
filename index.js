6dc222d606670604c225de572980ddd63f055a9f
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-01-15 13:04:14 +0900

    1.34 r:01

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 // - C_COMPLETE_DAILY_EVENT
 // - C_COMPLETE_EXTRA_EVENT
 // - S_COMPLETE_EVENT_MATCHING_QUEST
+// - S_LOGIN
 
-// Version 1.34 r:00
+// Version 1.34 r:01
 
 module.exports = function AutoVanguard(d) {
 
 	let enable = true
 
-	// for self ; comment out for regular usage
-	/*d.hook('S_LOGIN', (e) => {
-		(((e.templateId - 10101) % 100) != 5) ? enable = true : enable = false
-	})*/
+	// for self -- comment out for regular usage
+	/*
+		0 = Warrior, 1 = Lancer, 2 = Slayer, 3 = Berserker, 4 = Sorcerer, 5 = Archer,
+		6 = Priest, 7 = Mystic, 8 = Reaper, 9 = Gunner, 10 = Brawler, 11 = Ninja,
+		12 = Valkyrie
+	*/
+	d.hook('S_LOGIN', (e) => {
+		(((e.templateId - 10101) % 100) !== 5) ? enable = true : enable = false
+	})
 
 	// code
 	d.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (e) => {
@@ -33,13 +39,8 @@ module.exports = function AutoVanguard(d) {
 	try {
 		const Command = require('command')
 		const command = Command(d)
-		// KR
-		/*command.add('지령', () => {
-			enable = !enable
-			send(`${enable ? '실행되었습니다'.clr('56B4E9') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
-		})*/
-		// NA
-		command.add(['vg', 'vanguard', 'ㅍㅎ'], () => {
+		// toggle
+		command.add(['vanguard', 'vg', 'ㅍㅎ'], () => {
 			enable = !enable
 			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
