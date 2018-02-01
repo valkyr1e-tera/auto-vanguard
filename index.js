5b483742f2ca5f4a2e91e2527c6c4cd6fd3662d2
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-02-01 23:55:27 +0900

    1.35 r:00 Battleground support

--- index.js
+++ index.js
@@ -2,38 +2,67 @@
 // - C_COMPLETE_DAILY_EVENT
 // - C_COMPLETE_EXTRA_EVENT
 // - S_COMPLETE_EVENT_MATCHING_QUEST
+// - S_LOAD_TOPO
 // - S_LOGIN
 
-// Version 1.34 r:01
+// Version 1.35 r:00
+
+const battleground = [
+	102, // Wintera Snowfield (classic)
+	103, // Wintera Snowfield
+	110, // Gridiron || Underground Arena
+	111, // Iron Battleground
+	112, // Fraywind Canyon
+	116, // Corsairs' Stronghold
+	117, // Kumasylum
+	118, // Kumas Royale
+	119, // Coliseum
+]
 
 module.exports = function AutoVanguard(d) {
 
-	let enable = true
+	let enable = true,
+		hold = false,
+		questId = 0
 
 	// for self -- comment out for regular usage
-	/*
-		0 = Warrior, 1 = Lancer, 2 = Slayer, 3 = Berserker, 4 = Sorcerer, 5 = Archer,
-		6 = Priest, 7 = Mystic, 8 = Reaper, 9 = Gunner, 10 = Brawler, 11 = Ninja,
-		12 = Valkyrie
-	*/
-	//d.hook('S_LOGIN', (e) => {
-	//	(((e.templateId - 10101) % 100) !== 5) ? enable = true : enable = false
-	//})
+		// 0 = Warrior, 1 = Lancer, 2 = Slayer, 3 = Berserker, 4 = Sorcerer, 5 = Archer,
+		// 6 = Priest, 7 = Mystic, 8 = Reaper, 9 = Gunner, 10 = Brawler, 11 = Ninja,
+		//12 = Valkyrie
+	/* d.hook('S_LOGIN', (e) => {
+		(((e.templateId - 10101) % 100) !== 5) ? enable = true : enable = false
+	}) */
 
 	// code
+	d.hook('S_LOAD_TOPO', (e) => {
+		// check battleground
+		if (battleground.includes(e.zone)) { hold = true }
+		// else check if there is a quest to complete
+		else if (hold && questId !== 0) {
+			completeQuest()
+			hold = false
+		}
+	})
+
 	d.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (e) => {
 		if (!enable) return
+		questId = e.id
+		// check battleground
+		if (!hold) completeQuest()
+		return false
+	})
 
-		d.toServer('C_COMPLETE_DAILY_EVENT', { id: e.id })
+	//helper
+	function completeQuest() {
+		d.toServer('C_COMPLETE_DAILY_EVENT', { id: questId })
 		try {
 			setTimeout(() => { d.toServer('C_COMPLETE_EXTRA_EVENT', { type: 0 }) }, 500)
 			setTimeout(() => { d.toServer('C_COMPLETE_EXTRA_EVENT', { type: 1 }) }, 500)
 		} catch (e) {
 			// do nothing
 		}
-
-		return false
-	})
+		questId = 0
+	}
 
 	// command
 	try {
