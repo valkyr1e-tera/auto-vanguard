b38151bff2cbbe7cf435d51ac7a2ff7a3610c891
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 22:02:57 +0900

    1.36 r:00

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
-// OPCODE REQUIRED :
-// - C_COMPLETE_DAILY_EVENT
-// - C_COMPLETE_EXTRA_EVENT
-// - S_COMPLETE_EVENT_MATCHING_QUEST
-// - S_LOAD_TOPO
-// - S_LOGIN
+// Version 1.36 r:00
 
-// Version 1.35 r:01
+const Command = require('command')
+const battleground = require('./battleground.js')
+const config = require('./config.json')
 
-const battleground = [
-	102, // Wintera Snowfield (classic)
-	103, // Wintera Snowfield
-	110, // Gridiron || Underground Arena
-	111, // Iron Battleground
-	112, // Fraywind Canyon
-	116, // Corsairs' Stronghold
-	117, // Kumasylum
-	118, // Kumas Royale
-	119, // Coliseum
-]
+// credit : https://github.com/Some-AV-Popo
+String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
 
 module.exports = function AutoVanguard(d) {
+	const command = Command(d)
 
-	let enable = true,
-		hold = false,
+	let enable = config.enable
+
+	let hold = false,
 		questId = 0
 
 	// for self -- comment out for regular usage
-		// 0 = Warrior, 1 = Lancer, 2 = Slayer, 3 = Berserker, 4 = Sorcerer, 5 = Archer,
-		// 6 = Priest, 7 = Mystic, 8 = Reaper, 9 = Gunner, 10 = Brawler, 11 = Ninja,
-		//12 = Valkyrie
+	// 0 = Warrior, 1 = Lancer, 2 = Slayer, 3 = Berserker, 4 = Sorcerer, 5 = Archer,
+	// 6 = Priest, 7 = Mystic, 8 = Reaper, 9 = Gunner, 10 = Brawler, 11 = Ninja,
+	//12 = Valkyrie
 	/* d.hook('S_LOGIN', (e) => {
 		(((e.templateId - 10101) % 100) !== 5) ? enable = true : enable = false
 	}) */
 
 	// code
+	// if in battleground, hold completion until openworld
+	// else check if there is a quest to complete
 	d.hook('S_LOAD_TOPO', (e) => {
-		// check battleground
 		if (battleground.includes(e.zone)) { hold = true }
-		// else check if there is a quest to complete
-		else if (hold && questId !== 0) {
-			completeQuest()
-			hold = false
-		}
+		else if (hold && questId !== 0) { completeQuest(); hold = false }
 	})
 
+	// if not in battleground, complete vangaurd quest
+	// otherwise, hold questId for later completion
 	d.hook('S_COMPLETE_EVENT_MATCHING_QUEST', (e) => {
 		if (!enable) return
 		questId = e.id
-		// check battleground
 		if (!hold) completeQuest()
 		return false
 	})
 
 	//helper
+	// rudimentary attempt to complete both extra events
 	function completeQuest() {
 		d.toServer('C_COMPLETE_DAILY_EVENT', { id: questId })
 		try {
@@ -65,18 +54,11 @@ module.exports = function AutoVanguard(d) {
 	}
 
 	// command
-	try {
-		const Command = require('command')
-		const command = Command(d)
-		// toggle
-		command.add(['vanguard', 'vg', 'ㅍㅎ'], () => {
-			enable = !enable
-			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
-		})
-		function send(msg) { command.message(`[auto-vanguard] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- auto-vanguard module --`) }
+	// toggle
+	command.add(['vanguard', 'vg', 'ㅍㅎ'], () => {
+		enable = !enable
+		send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+	})
+	function send(msg) { command.message(`[auto-vanguard] : ` + msg) }
 
 }
-
-// credit : https://github.com/Some-AV-Popo
-String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
